a5e261b1ccf0a042cee338dc200a32aa4f0ab5c8
UtkarshSuman <utkarshsuman7@gmail.com>
2026-06-18 17:24:28 +0530

    Fix file and folder casing

---
+++ src/components/sections/Codingprofiles/Codingprofilesdata.js
@@ -0,0 +1,66 @@
+
+const codingProfilesData = {
+  sectionTitle: "My Coding Profiles",
+
+  profiles: [
+    {
+      id: "leetcode",
+      platform: "LeetCode",
+      description: "Solved over 189 questions with a streak of 150 days.",
+      link: "https://leetcode.com/u/UtkarshSuman/",
+      btnText: "Visit LeetCode",
+      logoUrl: "/images/leetcodeprior.png",
+      screenshotUrl: "/images/leetcode.webp",
+      accentColor: "#f89f1b",
+      stats: [
+        { label: "Solved", value: "167" },
+        { label: "Streak", value: "140+" },
+      ],
+    },
+    {
+      id: "codeforces",
+      platform: "Codeforces",
+      description: "Div 4, Constantly solving questions to improve logic building.",
+      link: "https://codeforces.com/profile/Utkarshsuman",
+      btnText: "Visit Codeforces",
+      logoUrl: "/images/codeforcesss.png",
+      screenshotUrl: "/images/codeforceslogo.png",
+      accentColor: "#354091d3",
+      stats: [
+        { label: "Rating", value: "368" },
+        { label: "Div", value: "4" },
+      ],
+    },
+    {
+      id: "gfg",
+      platform: "GeeksforGeeks",
+      description: "Institute Rank 2000, solving mcq, development and tests.",
+      link: "https://www.geeksforgeeks.org/profile/utkarshz0gk?tab=activity",
+      btnText: "Visit GeeksforGeeks",
+      logoUrl: "/images/gfgss.png",
+      screenshotUrl: "/images/gfg.svg",
+      accentColor: "#2f8d46",
+      // stats: [
+      //   { label: "Rank", value: "0" },
+      //   { label: "Streak", value: "0d" },
+      // ],
+    },
+    
+    {
+      id: "ninjas",
+      platform: "Coding Ninjas",
+      description: "Exploring data structures through hands-on challenges.",
+      link: "https://www.naukri.com/code360/profile/ed9625a5-4705-4709-8e9d-e96a04a2ee0a",
+      btnText: "Visit Coding Ninjas",
+      logoUrl: "/images/codingninjass.png",
+      screenshotUrl: "/images/codingninjalogo.webp",
+      accentColor: "#ff5722",
+      // stats: [
+      //   { label: "MCQs", value: "1+" },
+      //   { label: "Streak", value: "1d" },
+      // ],
+    },
+  ],
+};
+
+export default codingProfilesData;
---
+++ src/components/sections/Projectscard/projectdata.js
@@ -0,0 +1,94 @@
+const projects = [
+  {
+    id: 1,
+    title: "JokesBox",
+    description:
+      "Share jokes, discover new ones, and interact with others in real time, share, laugh and connect.",
+    image:
+      "/images/jokebox.png",
+    popLabel: "jokes & jokes",       // small label shown on image pop-out
+    liveLink: "https://jokesbox.vercel.app/",
+    githubLink: "https://github.com/UtkarshSuman/API",
+    techStack: [
+      { name: "React", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
+      { name: "JavaScript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+      { name: "Node.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
+      { name: "Vercel", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Mini-Postman",
+    description:
+      "A minimal Postman-like tool to send HTTP requests, view live responses, and debug APIs with ease.",
+    image:
+      "/images/minipostman.png",
+    popLabel: "mini-postman",
+    liveLink: "https://github.com/UtkarshSuman/Mini-Postman-App",
+    githubLink: "https://github.com/UtkarshSuman/Mini-Postman-App",
+    techStack: [
+      { name: "Node.js", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
+      {
+        name: "Express.js",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" 
+      },
+      {
+  name: "REST API",
+  icon: "https://cdn-icons-png.flaticon.com/512/2165/2165004.png"
+},
+      
+    ],
+  },
+  {
+    id: 3,
+    title: "Tic-Tac-Toe",
+    description:
+      "Tic-Tac-Toe Game with Dynamic Gameplay and Real-Time Updates using DOM manipulation.",
+    image:
+      "/images/tictactoe.png",
+    popLabel: "Game ",
+    liveLink: "https://github.com/UtkarshSuman/TIC-TAC-TOE-GAME",
+    githubLink: "https://github.com/UtkarshSuman/TIC-TAC-TOE-GAME",
+    techStack: [
+  { 
+    name: "HTML5", 
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" 
+  },
+  { 
+    name: "CSS3", 
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" 
+  },
+  { 
+    name: "JavaScript", 
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" 
+  }
+],
+  },
+  {
+    id: 4,
+    title: "Task Manager",
+    description:
+      "Manage your tasks efficiently and stay organized with secure and seamless tracking.",
+    image:
+      "/images/taskmanager2.png",
+    popLabel: "Manage Task",
+    liveLink: "https://github.com/UtkarshSuman/Task-Manager",
+    githubLink: "https://github.com/UtkarshSuman/Task-Manager",
+    techStack: [
+  { 
+    name: "Node.js", 
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" 
+  },
+  { 
+    name: "Express.js", 
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" 
+  },
+  { 
+    name: "PostgreSQL", 
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" 
+  }
+],
+  },
+];
+
+export default projects;
